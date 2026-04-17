e743531bda15d88268cdc7a8602ed9d9097757ee
robertsaitov <robert.saitov30@gmail.com>
2026-04-16 23:06:04 -0500

    dont generate vs code theme changer tmp at home dir

--- hyprland/.config/hypr/scripts/themeswitch.sh
+++ hyprland/.config/hypr/scripts/themeswitch.sh
@@ -92,7 +92,8 @@ if [ ! -z "$(grep '^1|' "$ThemeCtl" | awk -F '|' '{print $3}')" ] ; then
         code --install-extension "${codex}" 2> /dev/null
     fi
     codet=$(grep '^1|' "$ThemeCtl" | awk -F '|' '{print $3}' | cut -d '~' -f 2)
-    jq --arg codet "${codet}" '.["workbench.colorTheme"] |= $codet' "$ConfDir/Code/User/settings.json" > tmpvsc && mv tmpvsc "$ConfDir/Code/User/settings.json"
+    tmpvsc="$ConfDir/Code/User/settings.json.tmp"
+    jq --arg codet "${codet}" '.["workbench.colorTheme"] |= $codet' "$ConfDir/Code/User/settings.json" > "$tmpvsc" && mv "$tmpvsc" "$ConfDir/Code/User/settings.json"
 fi
 
 
